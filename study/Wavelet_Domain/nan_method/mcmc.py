fce90fa33434ef2b67c4708b8ddeda34d434888f
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-01-21 11:37:55 +1300

    move corner plotting to mcmc

--- study/Wavelet_Domain/nan_method/mcmc.py
+++ study/Wavelet_Domain/nan_method/mcmc.py
@@ -5,6 +5,30 @@ from gap_study_utils.mcmc_runner import run_mcmc
 from gap_study_utils.constants import LN_A_TRUE, LN_F_TRUE, LN_FDOT_TRUE
 import numpy as np
 
+import arviz as az
+import numpy as np
+
+from gap_study_utils.pygtc import plotGTC
+from gap_study_utils.constants import TRUES
+
+FNAMES = {
+    "basic": "out_mcmc/basic/emcee_chain.nc",
+    "gap": "out_mcmc/gap/emcee_chain.nc",
+    "noise": "out_mcmc/noise/emcee_chain.nc",
+    "gap+noise": "out_mcmc/gap+noise/emcee_chain.nc",
+}
+
+CORNER_KWGS = dict(
+    truths=TRUES,
+    paramNames=["lnA", "lnF", "lnFdot"],
+    figureSize="MNRAS_page",
+    customLegendFont={"size": 20},
+)
+
+
+
+
+
 np.random.seed(1234)
 
 OUTDIR = "out_mcmc"
@@ -12,12 +36,8 @@ os.makedirs(OUTDIR, exist_ok=True)
 NITER = 250
 DT = 20
 TMAX = 327_680
-# GAPS = [
-#     [TMAX * 0.22, TMAX * 0.3],
-#     [TMAX * 0.52, TMAX * 0.63],
-# ]
+
 GAPS = [
-    # [TMAX * 0.22, TMAX * 0.3],
     [TMAX * 0.499999, TMAX * 0.50001],
 ]
 
@@ -32,14 +52,37 @@ common_kwgs = dict(
 )
 
 
-if __name__ == "__main__":
-    run_mcmc(
-        gap_ranges=GAPS,
-        noise_realisation=True,
-        outdir=f"{OUTDIR}/gap+noise",
-        **common_kwgs
-    )
+def read_inference_data(file_name)->np.ndarray:
+    res = az.from_netcdf(file_name)
+    return az.sel_utils.xarray_to_ndarray(res.posterior)[1].T
 
+
+def plot_corner():
+    results = {k: read_inference_data(v) for k, v in FNAMES.items()}
+    fig = plotGTC(
+        chains=[results["basic"], results["noise"]],
+        chainLabels=["Basic", "Noise"],
+        **CORNER_KWGS
+    )
+    fig.savefig("corner_basic.png")
+    fig = plotGTC(
+        chains=[results["gap"], results["gap+noise"]],
+        chainLabels=["Gap", "Gap+noise"],
+        **CORNER_KWGS
+    )
+    fig.savefig("corner_gap.png")
+
+
+
+
+
+if __name__ == "__main__":
+    # run_mcmc(
+    #     gap_ranges=GAPS,
+    #     noise_realisation=True,
+    #     outdir=f"{OUTDIR}/gap+noise",
+    #     **common_kwgs
+    # )
     # run_mcmc(
     #     gap_ranges=None,
     #     noise_realisation=True,
@@ -59,3 +102,5 @@ if __name__ == "__main__":
     #     outdir=f"{OUTDIR}/gap",
     #     **common_kwgs
     # )
+
+    plot_corner()
