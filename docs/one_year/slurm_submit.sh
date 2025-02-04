0d8337f6673666408e209215a1c68fc881d97f5b
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-02-04 17:13:34 +1300

    add slurm submission script

--- docs/one_year/slurm_submit.sh
+++ docs/one_year/slurm_submit.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 #
 #SBATCH --job-name=one_year_lisa
-#SBATCH --output=logs/job_%A_%a.out
-#SBATCH --error=logs/job_%A_%a.err
-#SBATCH --time=00:100:00
+#SBATCH --output=/fred/oz980/avajpeyi/projects/pywavelet/gaps_study/docs/one_year/logs/job_%A_%a.out
+#SBATCH --error=/fred/oz980/avajpeyi/projects/pywavelet/gaps_study/docs/one_year/logs/job_%A_%a.err
+#SBATCH --time=00:20:00
 #SBATCH --array=0-3
 #SBATCH --mem=6G
 #SBATCH --cpus-per-task=4
 
-ml gcc/12.3.0 numpy/1.25.1-scipy-bundle-2023.07 && source /fred/oz101/avajpeyi/compas_env/bin/activate
+ml python-scientific/3.11.3-foss-2023a
+source /fred/oz980/avajpeyi/projects/pywavelet/venv/bin/activate
 
-readarray -t gen_cmd < '/fred/oz101/avajpeyi/code/pipeline/studies/one_param/pp_test/out_aSF/gen_cmd.txt'
-readarray -t analy_cmd < '/fred/oz101/avajpeyi/code/pipeline/studies/one_param/pp_test/out_aSF/analy_cmd.txt'
+readarray -t jobs < '/fred/oz980/avajpeyi/projects/pywavelet/gaps_study/docs/one_year/slurm_jobs.txt'
 
-echo "Mock cmd: ${gen_cmd[$SLURM_ARRAY_TASK_ID]}"
-echo "Surrogate cmd: ${analy_cmd[$SLURM_ARRAY_TASK_ID]}"
-eval "${gen_cmd[$SLURM_ARRAY_TASK_ID]}"
-eval "${analy_cmd[$SLURM_ARRAY_TASK_ID]}"
+echo "job cmd: ${jobs[$SLURM_ARRAY_TASK_ID]}"
+eval "${jobs[$SLURM_ARRAY_TASK_ID]}"
